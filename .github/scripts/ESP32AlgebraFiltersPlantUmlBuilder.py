fa93785d4b53d53db11b5b6137075c897dc5ad8c
robinZERTY <robinlecouffe@gmail.com>
2024-11-22 20:37:39 +0100

    update workflows

--- .github/scripts/ESP32AlgebraFiltersPlantUmlBuilder.py
+++ .github/scripts/ESP32AlgebraFiltersPlantUmlBuilder.py
@@ -3,8 +3,8 @@
 import os
 import hpp2plantuml
 
-folder2watch = os.path.join("lib","ESP32AlgebraFilters")
-puml_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..","..", folder2watch, "docs","classDiagram.puml")
+folder2watch = os.path.join("include")
+puml_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..","..", "docs","classDiagram.puml")
 
 hppFiles = [os.path.join(root, name) for root, dirs, files in os.walk(folder2watch) for name in files if name.endswith(".hpp")]
 hpp2plantuml.CreatePlantUMLFile(hppFiles, puml_file_path)
